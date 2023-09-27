20533b0b14766a3a8ed1585407d8e1349e1dfae9
MariyaN23 <sellaite505@gmail.com>
2023-09-27 16:06:44 +0300

    counter set button

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './Counter.module.css'
 import {Button} from './Button';
 
@@ -7,9 +7,12 @@ export const Counter = () => {
     const [number, setNumber] = useState<number>(0)
     const [disableButton, setDisableButton] = useState<boolean>(false)
 
+    const [maxValue, setMaxValue] = useState(5)
+    const [startValue, setStartValue] = useState(0)
+
     const numberChange = () => {
         setNumber(number + 1);
-        (number < 4 ? setDisableButton(false) : setDisableButton(true))
+        (number < maxValue - 1 ? setDisableButton(false) : setDisableButton(true))
     }
 
     const numberReset = () => {
@@ -17,30 +20,50 @@ export const Counter = () => {
         setDisableButton(false)
     }
 
-
-    const [maxValue,setMaxValue] = useState(0)
-    const [startValue,setStartValue] = useState(0)
-
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>)=>{
-        if (Number(e.currentTarget.value)<=25&&Number(e.currentTarget.value)>=-1) {
-            setMaxValue(Number(e.currentTarget.value))
-        }
+    const changeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(Number(e.currentTarget.value))
     }
 
+    useEffect(()=> {
+        localStorage.setItem('setMaxValue', JSON.stringify(maxValue))
+    }, [maxValue])
+
+    useEffect(()=> {
+        localStorage.setItem('setStartValue', JSON.stringify(startValue))
+    }, [startValue])
+
+    const changeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setStartValue(Number(e.currentTarget.value))
+    }
+
+    /*useEffect(()=> {
+        let startValueLocal = localStorage.getItem('setStartValue')
+        if (startValueLocal) {
+            let newStartValue = JSON.parse(startValueLocal)
+            setStartValue(newStartValue)
+        }
+    }, [])*/
+
+    const setButton = () => {
+        if (maxValue > startValue && startValue >= 0) {
+            setNumber(startValue)
+        }
+    }
 
     return (
         <div className={s.wrapper}>
             <div className={s.counter}>
                 <div className={s.settings}>
-                    <div>Max value:<input onChange={onChangeHandler} value={maxValue} type={'number'}/></div>
-                    <div>Start value:<input value={startValue} type={'number'}/></div>
+                    <div>Max value:<input onChange={changeMaxValue} value={maxValue} type={'number'}/></div>
+                    <div>Start value:<input onChange={changeStartValue} value={startValue} type={'number'}/></div>
                 </div>
                 <div className={s.buttons}>
-                    <Button name={'set'} callBack={()=>{}} disabled={false}/>
+                    <Button name={'set'} callBack={setButton}
+                            disabled={maxValue <= startValue || startValue < 0 || maxValue < 0}/>
                 </div>
             </div>
             <div className={s.counter}>
-                <div className={number === 5 ? (`${s.tabloChange} + ${s.tablo}`) : s.tablo}>{number}</div>
+                <div className={number === maxValue ? (`${s.tabloChange} + ${s.tablo}`) : s.tablo}>{number}</div>
                 <div className={s.buttons}>
                     <Button name={'inc'} callBack={numberChange} disabled={disableButton}/>
                     <Button name={'reset'} callBack={numberReset} disabled={number === 0}/>
